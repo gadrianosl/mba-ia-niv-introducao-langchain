2e6897d06c2d9f97c76912eff8dd5023e4b7aab8
Adriano <gadrianosl@gmail.com>
2025-08-28 13:57:38 -0300

    Ajuste no chat template para usar o gemini e usar portugues do brasil

--- 1-fundamentos/4-chat-prompt-template.py
+++ 1-fundamentos/4-chat-prompt-template.py
@@ -1,18 +1,18 @@
 from langchain.prompts import ChatPromptTemplate
-from langchain_openai import ChatOpenAI
+from langchain.chat_models import init_chat_model
 from dotenv import load_dotenv
 load_dotenv()
 
-system = ("system", "you are an assistant that answers questions in a {style} style")
+system = ("system", "você é um assistente que responde perguntas em um estilo {style}. Sempre responda em português do Brasil")
 user = ("user", "{question}")
 
 chat_prompt = ChatPromptTemplate([system, user])
 
-messages = chat_prompt.format_messages(style="funny", question="Who is Alan Turing?")
+messages = chat_prompt.format_messages(style="divertido", question="Quem foi Alan Turing?")
 
 for msg in messages:
     print(f"{msg.type}: {msg.content}")
 
-model = ChatOpenAI(model="gpt-5-mini", temperature=0.5)
+model = init_chat_model(model="gemini-2.5-flash", model_provider="google_genai")
 result = model.invoke(messages)
 print(result.content)
