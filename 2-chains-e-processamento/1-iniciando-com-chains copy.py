467f62d15522f99468001ab02cd0c21ab51c4384
Adriano <gadrianosl@gmail.com>
2025-08-30 14:55:57 -0300

    Ajuste no primeiro chain para responder em portugues e usar o gemini

--- 2-chains-e-processamento/1-iniciando-com-chains copy.py
+++ 2-chains-e-processamento/1-iniciando-com-chains copy.py
@@ -1,16 +1,16 @@
 from langchain.prompts import PromptTemplate
-from langchain_openai import ChatOpenAI
+from langchain.chat_models import init_chat_model
 from dotenv import load_dotenv
 load_dotenv()
 
 question_template = PromptTemplate(
     input_variables=["name"],
-    template="Hi, I'm {name}! Tell me a joke with my name!"
+    template="Olá, eu sou {name}! Me conte uma piada usando meu nome! Responda em português do Brasil."
 )
 
-model = ChatOpenAI(model="gpt-5-mini", temperature=0.5)
+model = init_chat_model(model="gemini-2.5-flash", model_provider="google_genai")
 
 chain = question_template | model
 
-result = chain.invoke({"name": "Wesley"})
+result = chain.invoke({"name": "Adriano"})
 print(result.content)
